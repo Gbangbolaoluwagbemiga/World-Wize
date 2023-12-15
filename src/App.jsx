5c7c307b3016c36d650a8c73467ab358a7409421
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-15 03:53:08 +0100

    No bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Product from './pages/Product';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Product />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
