afefe9579e58ef6dd23ab0aae9c6d4cea3daae22
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-15 03:36:26 +0100

    Router glitch

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,3 @@ function App() {
 }
 
 export default App;
-
-// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import Product from './pages/Product';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Product />} />
-//         <Route path="product" element={<Product />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
