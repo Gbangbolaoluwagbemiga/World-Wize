af6303cf93bbf7c42994593fa3120762d388984b
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-17 21:01:46 +0100

    Today init

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,5 +1,24 @@
+import {NavLink} from 'react-router-dom';
+
 function PageNav() {
-  return <div></div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/product'}>Product</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default PageNav;
