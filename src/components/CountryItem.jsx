c6df99da805e03bd8c99e741e0809b61bcf4dff0
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-22 20:08:43 +0100

    App bug fixed

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -2,10 +2,12 @@ import styles from './CountryItem.module.css';
 
 function CountryItem({country}) {
   return (
-    <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
-      <span>{country.country}</span>
-    </li>
+    // <li className={styles.countryItem}>
+    //   <span>{country.emoji}</span>
+    //   <span>{country.country}</span>
+    // </li>
+
+    <p>HIIIIIII</p>
   );
 }
 
