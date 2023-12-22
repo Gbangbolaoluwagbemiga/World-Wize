31288bba5360afb9533151040c9afe19ac66a2cc
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-22 19:08:48 +0100

    cityItem component list

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,16 @@
 import styles from './CityItem.module.css';
 
 function CityItem() {
-  return <div></div>;
+  return (
+    <div className={styles.cityItem}>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
 
 export default CityItem;
