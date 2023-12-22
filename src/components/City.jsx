0a722d82a259c23b4f913e3a70bf6d50e74f413e
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-22 19:37:48 +0100

    City Item implementation

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,12 +1,5 @@
 import styles from './City.module.css';
-
-const formatDate = date =>
-  new Intl.DateTimeFormat('en', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    weekday: 'long',
-  }).format(new Date(date));
+import CityItem from './CityItem';
 
 function City({cities}) {
   // TEMP DATA
@@ -18,46 +11,47 @@ function City({cities}) {
   // };
   console.log(cities);
 
-  // const {cityName, emoji, date, notes} = currentCity;
   return (
     <>
       {cities.map(city => (
-        <div className={styles.city} key={city.id}>
-          <div className={styles.row}>
-            <h6>City name</h6>
-            <h3>
-              <span>{city.emoji}</span> {city.cityName}
-            </h3>
-          </div>
-
-          <div className={styles.row}>
-            <h6>You went to {city.cityName} on</h6>
-            <p>{formatDate(city.date || null)}</p>
-          </div>
-
-          {city.notes && (
-            <div className={styles.row}>
-              <h6>Your notes</h6>
-              <p>{city.notes}</p>
-            </div>
-          )}
-
-          <div className={styles.row}>
-            <h6>Learn more</h6>
-            <a
-              href={`https://en.wikipedia.org/wiki/${city.cityName}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Check out {city.cityName} on Wikipedia &rarr;
-            </a>
-          </div>
-
-          <div>{/* <ButtonBack /> */}</div>
-        </div>
+        <CityItem key={city.id} city={city} />
       ))}
     </>
   );
 }
+/*{ <div className={styles.city} key={city.id}>
+<div className={styles.row}>
+  <h6>City name</h6>
+  <h3>
+    <span>{city.emoji}</span> {city.cityName}
+  </h3>
+</div>
+
+<div className={styles.row}>
+  <h6>You went to {city.cityName} on</h6>
+  <p>{formatDate(city.date || null)}</p>
+</div>
+
+{city.notes && (
+  <div className={styles.row}>
+    <h6>Your notes</h6>
+    <p>{city.notes}</p>
+  </div>
+)}
+
+<div className={styles.row}>
+  <h6>Learn more</h6>
+  <a
+    href={`https://en.wikipedia.org/wiki/${city.cityName}`}
+    target="_blank"
+    rel="noreferrer"
+  >
+    Check out {city.cityName} on Wikipedia &rarr;
+  </a>
+</div>
+
+<div>{/* <ButtonBack /> */
+// }</div>
+// </div> */}
 
 export default City;
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,14 +1,22 @@
 import styles from './CityItem.module.css';
+const formatDate = date =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date(date));
 
-function CityItem() {
+function CityItem({city}) {
+  const {cityName, emoji, date, notes} = city;
+
   return (
     <div className={styles.cityItem}>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <li>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}> &times;</button>
+      </li>
     </div>
   );
 }
