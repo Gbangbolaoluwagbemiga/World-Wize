6eb89d0c18c2d4d5c2a8f89db100f9a01d72e1ee
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-23 01:58:19 +0100

    CityList component implementation

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,10 @@
 import CityItem from './CityItem';
 import styles from './CityList.module.css';
+import Message from './Message';
 
 function CityList({cities}) {
+  if (!cities.length)
+    return <Message message={`No cities added, Click the map to add a city`} />;
   return (
     <ul className={styles.cityList}>
       {cities.map(city => (
