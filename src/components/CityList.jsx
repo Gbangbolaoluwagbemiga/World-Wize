e361788abfe15957862047734d370e55d2a7b4de
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-28 15:59:17 +0100

    CitylIst refactored

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,11 @@
+import {useContext} from 'react';
 import CityItem from './CityItem';
 import styles from './CityList.module.css';
 import Message from './Message';
 
-function CityList({cities}) {
+function CityList() {
+  const {cities} = useContext();
+
   if (!cities.length)
     return <Message message={`No cities added, Click the map to add a city`} />;
   return (
