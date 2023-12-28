1a1339c16e290e76ea101ef18d93afcc3e045eb8
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-28 17:36:06 +0100

    Hamburger active

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -4,21 +4,41 @@ import Logo from './Logo';
 
 function PageNav() {
   return (
-    <nav className={styles.nav}>
-      <Logo />
-      <ul>
-        <li>
-          <NavLink to={'/pricing'}>Pricing</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/product'}>Product</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/login'} className={styles.ctaLink}>
-            Login
-          </NavLink>
-        </li>
-      </ul>
+    // <nav className={styles.nav}>
+    //   <Logo />
+
+    // </nav>
+
+    <nav className={`navbar navbar-expand-lg ${styles.nav}`}>
+      <div className="container-fluid">
+        <Logo />
+        <button
+          className="navbar-toggler hamburger"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse ms-md-auto" id="navbarNav">
+          <ul className="navbar-nav ms-md-auto mb-2 mb-lg-0">
+            <li className="nav-item ">
+              <NavLink to={'/pricing'}>Pricing</NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to={'/product'}>Product</NavLink>
+            </li>
+            <li className="nav-item ">
+              <NavLink to={'/login'} className={styles.ctaLink}>
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
