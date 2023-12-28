d975969bb2d7e770d4e6ffae2f46d297907a82e4
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-28 19:05:54 +0100

    refactoring

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,23 +1,17 @@
-// import styles from './SideBar.module.css';
+import styles from './SideBar.module.css';
 import Logo from './Logo';
 import AppNav from './AppNav';
 import {Outlet} from 'react-router-dom';
 
 function SideBar() {
   return (
-    <div
-    // className={styles.sidebar}
-    >
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
       <Outlet />
-      <footer
-      // className={styles.footer}
-      >
-        <p
-        // className={styles.copyright}
-        >
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
           {' '}
           &copy; Copyright {new Date().getFullYear()} by Worldwise Inc.
         </p>
