b8d8f9895292d1dfb70fbcfc120e898778b6c8c5
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-28 19:15:09 +0100

    Sidebar module exported

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,17 +1,21 @@
-import styles from './SideBar.module.css';
+// import styles from './SideBar.module.css';
 import Logo from './Logo';
 import AppNav from './AppNav';
 import {Outlet} from 'react-router-dom';
 
 function SideBar() {
   return (
-    <div className={styles.sidebar}>
+    // <div className={styles.sidebar}>
+    <div className={`sidebar`}>
       <Logo />
       <AppNav />
 
       <Outlet />
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
+      {/* <footer className={styles.footer}>
+        <p className={styles.copyright}> */}
+
+      <footer className={`footer`}>
+        <p className={`copyright`}>
           {' '}
           &copy; Copyright {new Date().getFullYear()} by Worldwise Inc.
         </p>
