0446cb83027f731b82932e6d1dbace424a4b4621
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-31 18:04:56 +0100

    Map successfully loaded

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,26 @@
 import {useNavigate, useParams, useSearchParams} from 'react-router-dom';
 import styles from './Map.module.css';
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import {useState} from 'react';
 
 function Map() {
   const navigate = useNavigate();
-
+  const [position, setPosition] = useState([40, 17]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
-
-  // const position = [lng, lat];
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
       <MapContainer
         center={position}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           <Popup>
