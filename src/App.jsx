0906b65d72208d6a40859a331fa3713788e1ead1
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-11 21:48:06 +0100

    Suspense added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {lazy} from 'react';
+import {Suspense, lazy} from 'react';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import {CitiesProvider} from './context/CitiesContext';
 import {AuthProvider} from './context/AuthContext';
@@ -8,6 +8,9 @@ const Pricing = lazy(import('./pages/Pricing'));
 const Product = lazy(import('./pages/Product'));
 const Login = lazy(import('./pages/Login'));
 const AppLayout = lazy(import('./pages/AppLayout'));
+
+// dist/assets/index-58deac7a.css   35.50 kB │ gzip:   5.90 kB
+// dist/assets/index-66d5062d.js   533.76 kB │ gzip: 151.19 kB
 
 // import Product from './pages/Product';
 // import Homepage from './pages/Homepage';
@@ -27,20 +30,22 @@ function App() {
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
-            <Route path="product" element={<Product />} />
-            <Route path="product" element={<Product />} />
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="appLayout" element={<AppLayout />}>
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="pricing" element={<Pricing />} />
+            <Suspense>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
+              <Route path="product" element={<Product />} />
+              <Route path="product" element={<Product />} />
+              <Route path="*" element={<PageNotFound />} />
+              <Route path="appLayout" element={<AppLayout />}>
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="pricing" element={<Pricing />} />
+            </Suspense>
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
