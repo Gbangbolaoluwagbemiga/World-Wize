0d1ae65b8e4934c83366125016baf3955c9e13be
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-14 17:29:25 +0100

    Product page completed

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -19,22 +19,6 @@ export default function Product() {
             transform your adventures into unforgettable memories with our
             feature-packed travel companion.
           </p>
-          <p>
-            Features: Interactive World Map: Embark on a journey of
-            self-discovery as you visualize your travels on an interactive world
-            map. Pin every city you've explored and watch your personal
-            travelogue unfold with each click. Detailed City Statistics: Dive
-            deep into the heart of every destination. WorldWise provides
-            comprehensive statistics for each city you've visited, offering
-            insights into your travel habits and creating a personalized diary
-            of your globetrotting escapades. Effortless Tracking: Seamlessly
-            track your journey with our user-friendly interface. Mark the cities
-            you've conquered, reminisce about your favorite moments, and share
-            your travel stories with friends and family. Mobile Accessibility:
-            Carry your travel memories in your pocket! WorldWise is optimized
-            for mobile devices, allowing you to check your stats and explore new
-            destinations on the go.{' '}
-          </p>
         </div>
       </section>
     </main>
